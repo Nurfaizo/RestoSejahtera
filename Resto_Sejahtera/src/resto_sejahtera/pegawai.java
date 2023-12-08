60c69ca753e6e18bf30401fa31a7d001f118407f
M Nurfaiz <112399298+Nurfaizo@users.noreply.github.com>
2023-12-08 16:10:19 +0700

    Add files via upload

---
+++ Resto_Sejahtera/src/resto_sejahtera/pegawai.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package resto_sejahtera;
+
+/**
+ *
+ * @author Faiz
+ */
+public class pegawai {
+    
+}
