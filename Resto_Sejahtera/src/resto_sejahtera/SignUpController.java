b4370b18411772d25bf2dc6b5b6d1d8b8d3a0b69
FarhanDzakyA <112404911+FarhanDzakyA@users.noreply.github.com>
2023-12-14 13:42:56 +0700

    Add files via upload

---
+++ Resto_Sejahtera/src/resto_sejahtera/SignUpController.java
@@ -0,0 +1,31 @@
+package resto_sejahtera;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private Button btLogin;
+
+    @FXML
+    private PasswordField pfPassword;
+
+    @FXML
+    private TextField tfUsename;
+
+    @FXML
+    private TextField tfUsename1;
+
+    @FXML
+    private TextField tfUsename11;
+
+    @FXML
+    void Login(ActionEvent event) {
+
+    }
+
+}
