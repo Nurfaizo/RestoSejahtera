94fb44f23054569d82d4a08527dc559387c1be31
Leonardo1228e7ur <130635456+Leonardo1228e7ur@users.noreply.github.com>
2023-12-14 17:47:32 +0700

    Update KelolaMenuController.java

--- Resto_Sejahtera/src/resto_sejahtera/KelolaMenuController.java
+++ Resto_Sejahtera/src/resto_sejahtera/KelolaMenuController.java
@@ -1,1 +1,57 @@
+package resto_sejahtera;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class KelolaMenuController {
+
+    @FXML
+    private Button bthapus;
+
+    @FXML
+    private Button btkelolamenu;
+
+    @FXML
+    private Button bttambah;
+
+    @FXML
+    private Button btupdate;
+
+    @FXML
+    private TableColumn<?, ?> koldeskripsi;
+
+    @FXML
+    private TableColumn<?, ?> kolharga;
+
+    @FXML
+    private TableColumn<?, ?> kolid;
+
+    @FXML
+    private TableColumn<?, ?> kolnama;
+
+    @FXML
+    private TextField tfID;
+
+    @FXML
+    private TextField tfdeskripsi;
+
+    @FXML
+    private TextField tfharga;
+
+    @FXML
+    private TextField tfnama;
+
+    @FXML
+    private TableView<?> tvdata;
+
+    @FXML
+    void handleButtonAction(ActionEvent event) {
+
+    }
+
+}
 
